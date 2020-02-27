0062b1ba74cd4dc8793f646b0a92defbf9749380
hellrezistor <50468493+hellresistor@users.noreply.github.com>
2020-02-27 00:10:22 +0000

    Create 01-InstructionExec.sh

---
+++ Hows/01-InstructionExec.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+
+echo -e "DEBIAN WITH WHONIX ON USB COMPLETED INSTALLED \n Shutdown and Rest a Little.. OR PRESS <Enter> Key to Continue ;)"
+read -p
+
+echo "checking securely updates to Host"
+dist-upgrade
+
+cat 'EOF'
+#############################
+## BOOT/RUN Whonix Gateway ##
+#############################
+sudo apt-get-update-plus dist-upgrade && sudo apt-get clean
+
+#############################################################
+## Every time you run updates AND installing, NEED DO THIS ##
+#############################################################
+# Shutdown - Boot in Recovery Mode - Put Password #
+# type this commands:
+
+$ mount -o remount,ro /dev/sda1 /
+$ zerofree -v /dev/sda1 
+$ shutdown now
+
+# Take Snapshot and Start Gateway Again #
+##############################################################
+EOF
+
+read -p "AFTER Succefully runned of Whonix Gateway...
+Press <Enter> Key to Continue.."
+
+cat 'EOF'
+#################################
+## BOOT/RUN Whonix Workstation ##
+##   Execute this on Terminal  ##
+#################################
+sudo apt-get-update-plus dist-upgrade && sudo apt-get clean
+
+#############################################################
+## Every time you run updates AND installing, NEED DO THIS ##
+#############################################################
+# Shutdown - Boot in Recovery Mode - Put password #
+# type this commands:
+
+$ mount -o remount,ro /dev/sda1 /
+$ mount -o remount,ro /dev/sdb1 /home/user/storage 
+$ zerofree -v /dev/sda1 
+$ zerofree -v /dev/sdb1 
+$ shutdown now
+
+# Take Snapshot ONLY #
+##############################################################
+EOF
+
+read -p "### VERIFY WHONIXes VMs ARE ALL SHUTDOWN!!! ###"
+vbcompact
+
+sleep 2
