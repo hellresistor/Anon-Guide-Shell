8ca9083d67886cdaea0c8594a0f0e022311d0231
hellrezistor <50468493+hellresistor@users.noreply.github.com>
2020-02-27 00:27:48 +0000

    Update and rename Debian.sh to Debian-starter.sh

--- Debian/Debian-starter.sh
+++ Debian/Debian-starter.sh
@@ -2,7 +2,6 @@
 ##
 source .ChangeMe
 
-starter(){
 # root session #
 sudo -i
 
@@ -31,25 +30,3 @@ apt-get install -y linux-headers-amd64 software-properties-common cpu-checker
 echo "Checking VM Support..."
 sudo kvm-ok
 read -p -r "Press any key to continue"
-}
-
-ender(){
-sudo systemctl disable tor.service
-
-echo "alias dist-upgrade='sudo systemctl start tor.service && sleep 30 && sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get clean && sudo systemctl stop tor.service && test -f /var/run/reboot-required && tput setaf 1 && echo NEW SOFTWARE REQUIRES A REBOOT. && tput setaf 2 && echo Press ENTER to reboot your && read -sp computer: && echo If prompted by Debian, type your password and press ENTER. && sudo reboot'" >> .bashrc 
-echo "function apt-install() { sudo systemctl start tor.service; sleep 30; sudo apt-get update; sudo apt-get install "\$@"; sudo apt-get clean; sudo systemctl stop tor.service; }" >> .bashrc 
-echo "alias vbcompact='find ~/VirtualBox\ VMs/ -type f -mtime -1 -size +10M -name *.vdi -exec vboxmanage modifymedium --compact {} \;'" >> .bashrc
-
-source .bashrc
-dist-upgrade 
-
-read -p "Os Updated!
-Press <Enter> Key to Continue..."
-}
-
-starter
-. InstructionExec.sh
-ender
-
-echo "COMPLETED!!!!!!"
-exit
