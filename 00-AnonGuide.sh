677007286c6179c201ea5315b5122beac640864d
hellrezistor <50468493+hellresistor@users.noreply.github.com>
2020-02-29 09:21:38 +0000

    Update 00-AnonGuide.sh

--- 00-AnonGuide.sh
+++ 00-AnonGuide.sh
@@ -2,48 +2,7 @@
 ## Starter ##
 #############
 
-PANICX(){
-echo "Lets Config a PANIC PASSWORD ;)"
-sleep 2
-
-sudo apt install -y git make build-essential libpam0g-dev libssl1.1 libssl-dev
-git clone https://github.com/Lqp1/pam_duress
-# apt install libcurl4-openssl-dev libpam-cracklib ## check if REALLY needed installed..
-
-cd pam_duress
-make
-sudo make install
-make clean
-
-sudo cp /etc/pam.d/common-auth /etc/pam.d/common-auth.bck
-echo "auth    [success=3 default=ignore]      pam_unix.so nullok_secure
-auth    [success=2 default=ignore]      pam_duress.so disallow
-auth    sufficient                      pam_duress.so
-auth    requisite                       pam_deny.so
-auth    required                        pam_permit.so
-" | sudo tee -a /etc/pam.d/common-auth
-
-sudo ln -s /usr/lib/security /lib/security
-
-read -p -s "WRITE a Panic Password to your user: $USER" PANICPSWD
-
-if [ -z "$ScriptLoc" ]; then
- ScriptLoc="$PWD/pam_duress/examples/delete-all.sh"
-else
- read -p " Your User: $USER
- PanicPswd: $PANICPSWD
- Script: $ScriptLoc
- 
- Are you SURE?? .. <Enter> "
-fi
-
-sudo pam_duress_adduser $USER $PANICPSWD $ScriptLoc
-read -p "$USER Panic Password Created with execution script: $ScriptLoc
-Press <Enter> Key to continue"
-}
-
 MENU(){
-PANICX
 echo "Detecting OS ..." && sleep 1
 if [ -f /etc/os-release ]
 then
