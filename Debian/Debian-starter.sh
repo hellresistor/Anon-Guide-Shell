2b7bba3133f672683ceac2aa039c01d3e65bf22b
hellrezistor <50468493+hellresistor@users.noreply.github.com>
2020-03-11 05:57:45 +0000

    Update Debian-starter.sh

--- Debian/Debian-starter.sh
+++ Debian/Debian-starter.sh
@@ -81,6 +81,7 @@ ufw enable
 
 apt-get install tor apt-transport-tor
 cp /etc/apt/sources.list /etc/apt/sources.list.original 
+sudo sed -i 's|https|tor+https|g' /etc/apt/sources.list
 echo "deb tor+https://deb.debian.org/debian-security $ID_CODENAME/updates main contrib non-free" | sudo tee -a /etc/apt/sources.list
 apt-get update
 apt-get install -y linux-headers-$(dpkg --print-architecture) software-properties-common
