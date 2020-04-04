53ed19dc5917b5ab36b7a3f702b8803bd5d7e941
hellrezistor <50468493+hellresistor@users.noreply.github.com>
2020-04-05 00:32:50 +0100

    Update 00-AnonGuide.sh

--- 00-AnonGuide.sh
+++ 00-AnonGuide.sh
@@ -49,45 +49,48 @@ EOF
 sleep 1.5
 }
 
-DebianStarter(){
+DebianStarter() {
 
 echo "IF you REALLY are Using Anon-Guide...
 well.... you need answer this question..
   -- What CHAPTER have you walked? -- 
 1 - Chapter 2A
 2 - Chapter 2B" 
-read CHPTROPT
+read -r CHPTROPT
 
 if [ "$CHPTROPT" == "1" ];then
  echo "Are you Using this METHOD: Debian (Encrypted Boot USB)" && sleep 1
 elif [ "$CHPTROPT" == "2" ];then
  echo "Are you Using this METHOD: Debian (USB / Internal HDD) + BootKey (USB)" && sleep 1
 sudo dd if=/dev/urandom of=/keyfile bs=512 count=16
-sudo YourDeviceName=$(awk '{print $2}' /etc/crypttab) ### NEEED WORK ON THIS awk ....
+
+### NEEED WORK ON THIS awk ....
+YourDeviceName=$(sudo awk '{print $2}' /etc/crypttab) 
+
 sudo sed -i 's+none luks+/boot/keyfile.gpg luks,keyscript=/lib/cryptsetup/scripts/decrypt_gnupg+' /etc/crypttab
-sudo cryptsetup luksAddKey /dev/$YourDeviceName /keyfile
+sudo cryptsetup luksAddKey /dev/"$YourDeviceName" /keyfile
  echo "Set Password... Same has BOOT" && sleep 2
 sudo gpg -c --cipher-algo AES256 /keyfile 
 sudo mv /keyfile.gpg /boot/keyfile.gpg 
 sudo update-initramfs -u
-sudo cryptsetup luksKillSlot /dev/$YourDeviceName 0 --key-file /keyfile
+sudo cryptsetup luksKillSlot /dev/"$YourDeviceName" 0 --key-file /keyfile
 sudo shred -n 30 -uv /keyfile 
 
  echo "Chapter 2C Finished!!" 
- read -p "Press <Enter> key to continue..."
+ read -r -p "Press <Enter> key to continue..."
 else
  echo "FAIL COCKSUCKER" && exit 0
 fi
 
 echo "Disable SOUNd"
 gnome-control-center sound
-read -p "Press <Enter> key to continue..."
+read -r -p "Press <Enter> key to continue..."
 echo "Disable IPv6"
 gnome-control-center network
-read -p "Press <Enter> key to continue..."
+read -r -p "Press <Enter> key to continue..."
 echo "Disable History & Usage + Purge Trash"
 gnome-control-center privacy
-read -p "Press <Enter> key to continue..."
+read -r -p "Press <Enter> key to continue..."
 
 echo "net.ipv4.tcp_timestamps = 0" | sudo tee -a  /etc/sysctl.d/tcp_timestamps.conf
 sudo sysctl -p /etc/sysctl.d/tcp_timestamps.conf
@@ -111,11 +114,11 @@ sudo cp /etc/apt/sources.list /etc/apt/sources.list.original
 sudo sed -i 's|https|tor+https|g' /etc/apt/sources.list
 echo "deb tor+https://deb.debian.org/debian-security $ID_CODENAME/updates main contrib non-free" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get install -y linux-headers-$(dpkg --print-architecture) software-properties-common
+sudo apt-get install -y linux-headers-"$(dpkg --print-architecture)" software-properties-common
 
 git clone https://gist.github.com/hellresistor/e5a6d9cc3a138ac70603b6fdda7ea588
-chmod -R +x $PWD/e5a6d9cc3a138ac70603b6fdda7ea588/pam_duress_debianInst.sh
-bash $PWD/e5a6d9cc3a138ac70603b6fdda7ea588/pam_duress_debianInst.sh
+chmod -R +x "$PWD"/e5a6d9cc3a138ac70603b6fdda7ea588/pam_duress_debianInst.sh
+bash "$PWD"/e5a6d9cc3a138ac70603b6fdda7ea588/pam_duress_debianInst.sh
 
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.older
 echo "deb [arch=amd64] tor+https://download.virtualbox.org/virtualbox/debian $ID_CODENAME contrib" | sudo tee -a /etc/apt/sources.list
@@ -128,34 +131,33 @@ sudo sed -i '/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/cGRUB_CMDLINE_LINUX_DEFAULT="ip
 sudo update-grub
 }
 
-DebianVBOX(){
-cd $PWD/Downloads
+DebianVBOX() {
+cd "$PWD"/Downloads || exit
 echo "Get Whonix and Keys.."
-torsocks wget -c https://download.whonix.org/ova/$WNXVER/Whonix-$WNXFAC-$WNXVER.ova
-torsocks wget -c https://download.whonix.org/ova/$WNXVER/Whonix-$WNXFAC-$WNXVER.ova.asc
+torsocks wget -c https://download.whonix.org/ova/"$WNXVER"/Whonix-"$WNXFAC"-"$WNXVER".ova
+torsocks wget -c https://download.whonix.org/ova/"$WNXVER"/Whonix-"$WNXFAC"-"$WNXVER".ova.asc
 torsocks wget -c https://www.whonix.org/patrick.asc 
 
 echo "Verifing Keys..."
 gpg --keyid-format long --with-fingerprint patrick.asc 
 gpg --import patrick.asc 
-gpg --verify-options show-notations --verify Whonix-$WNXFAC-$WNXVER.ova.asc
+gpg --verify-options show-notations --verify Whonix-"$WNXFAC"-"$WNXVER".ova.asc
    
-vboxmanage import Whonix-$WNXFAC-$WNXVER.ova --vsys 0 --eula accept --vsys 1 --eula accept
-cd $PWD
-vboxmanage clonehd VirtualBox\ VMs/Whonix-Gateway-$WNXFAC/Whonix-$WNXFAC-$WNXVER-disk001.vmdk VirtualBox\ VMs/Whonix-Gateway-$WNXFAC/Whonix-$WNXFAC-$WNXVER-disk001.vdi --format VDI
-vboxmanage storageattach Whonix-Gateway-$WNXFAC --storagectl "Whonix-Gateway-$WNXFAC-sas" --port 0 --device 0 --type hdd --medium $PWD/VirtualBox\ VMs/Whonix-Gateway-$WNXFAC/Whonix-$WNXFAC-$WNXVER-disk001.vdi --mtype immutable
-   
-vboxmanage closemedium disk VirtualBox\ VMs/Whonix-Gateway-$WNXFAC/Whonix-$WNXFAC-$WNXVER-disk001.vmdk --delete
-vboxmanage clonehd VirtualBox\ VMs/Whonix-Workstation-$WNXFAC/Whonix-$WNXFAC-$WNXVER-disk002.vmdk VirtualBox\ VMs/Whonix-Workstation-$WNXFAC/Whonix-$WNXFAC-$WNXVER-disk001.vdi --format VDI
-vboxmanage storageattach Whonix-Workstation-$WNXFAC --storagectl "Whonix-Workstation-$WNXFAC-sas" --port 0 --device 0 --type hdd --medium $PWD/VirtualBox\ VMs/Whonix-Workstation-$WNXFAC/Whonix-$WNXFAC-$WNXVER-disk001.vdi --mtype immutable
-vboxmanage closemedium disk VirtualBox\ VMs/Whonix-Workstation-$WNXFAC/Whonix-$WNXFAC-$WNXVER-disk002.vmdk --delete
-vboxmanage createhd --filename VirtualBox\ VMs/Whonix-Workstation-$WNXFAC/Whonix-Workstation-$WNXFAC-storage.vdi --size 8192
-vboxmanage storageattach Whonix-Workstation-$WNXFAC --storagectl "Whonix-Workstation-$WNXFAC-sas" --port 1 --device 0 --type hdd --medium $PWD/VirtualBox\ VMs/Whonix-Workstation-$WNXFAC/Whonix-Workstation-$WNXFAC-storage.vdi --mtype writethrough
+vboxmanage import Whonix-"$WNXFAC-$WNXVER".ova --vsys 0 --eula accept --vsys 1 --eula accept
+cd "$PWD" || return
+vboxmanage clonehd VirtualBox\ VMs/Whonix-Gateway-"$WNXFAC"/Whonix-"$WNXFAC"-"$WNXVER"-disk001.vmdk VirtualBox\ VMs/Whonix-Gateway-"$WNXFAC"/Whonix-"$WNXFAC"-"$WNXVER"-disk001.vdi --format VDI
+vboxmanage storageattach Whonix-Gateway-"$WNXFAC" --storagectl "Whonix-Gateway-$WNXFAC-sas" --port 0 --device 0 --type hdd --medium "$PWD"/VirtualBox\ VMs/Whonix-Gateway-"$WNXFAC"/Whonix-"$WNXFAC"-"$WNXVER"-disk001.vdi --mtype immutable
+vboxmanage closemedium disk VirtualBox\ VMs/Whonix-Gateway-"$WNXFAC"/Whonix-"$WNXFAC"-"$WNXVER"-disk001.vmdk --delete
+vboxmanage clonehd VirtualBox\ VMs/Whonix-Workstation-"$WNXFAC"/Whonix-"$WNXFAC"-"$WNXVER"-disk002.vmdk VirtualBox\ VMs/Whonix-Workstation-"$WNXFAC"/Whonix-"$WNXFAC"-"$WNXVER"-disk001.vdi --format VDI
+vboxmanage storageattach Whonix-Workstation-"$WNXFAC" --storagectl "Whonix-Workstation-$WNXFAC-sas" --port 0 --device 0 --type hdd --medium "$PWD"/VirtualBox\ VMs/Whonix-Workstation-"$WNXFAC"/Whonix-"$WNXFAC"-"$WNXVER"-disk001.vdi --mtype immutable
+vboxmanage closemedium disk VirtualBox\ VMs/Whonix-Workstation-"$WNXFAC"/Whonix-"$WNXFAC"-"$WNXVER"-disk002.vmdk --delete
+vboxmanage createhd --filename VirtualBox\ VMs/Whonix-Workstation-"$WNXFAC"/Whonix-Workstation-"$WNXFAC"-storage.vdi --size 8192
+vboxmanage storageattach Whonix-Workstation-"$WNXFAC" --storagectl "Whonix-Workstation-$WNXFAC-sas" --port 1 --device 0 --type hdd --medium "$PWD"/VirtualBox\ VMs/Whonix-Workstation-"$WNXFAC"/Whonix-Workstation-"$WNXFAC"-storage.vdi --mtype writethrough
 
-rm $PWD/Download/*
+rm "$PWD"/Download/*
 }
 
-DebianQEMU(){
+DebianQEMU() {
 sudo apt install cpu-checker
 echo "Checking QeMu Support..."
 sudo kvm-ok
@@ -163,10 +165,11 @@ read -p -r "Press any key to continue"
 sudo apt install qemu qemu-kvm libvirt-bin libvirt-daemon-system libvirt-clients bridge-utils virt-manager libosinfo-bin git time curl apt-cacher-ng lsb-release fakeroot dpkg-dev build-essential devscripts gir1.2-spiceclientgtk-3.0
 
 echo "activating services..."
-sudo cat<<EOF>> /etc/libvirt/qemu.conf
+cat << EOF  | sudo tee -a /etc/libvirt/qemu.conf
 user = "root"
 group = "root"
 EOF
+
 sudo service libvirtd start
 sudo update-rc.d libvirtd enable
 sudo service libvirtd status
@@ -174,22 +177,23 @@ sudo addgroup "$(whoami)" libvirt
 sudo addgroup "$(whoami)" kvm
 
 echo "Downloading Whonix"
-cd $PWD/Downloads
-torsocks wget -c https://download.whonix.org/libvirt/$WNXVER/Whonix-$WNXFAC-$WNXVER.libvirt.xz
-torsocks wget -c https://download.whonix.org/libvirt/$WNXVER/Whonix-$WNXFAC-$WNXVER.libvirt.xz.asc
+cd "$PWD"/Downloads || exit
+torsocks wget -c https://download.whonix.org/libvirt/"$WNXVER"/Whonix-"$WNXFAC"-"$WNXVER".libvirt.xz
+torsocks wget -c https://download.whonix.org/libvirt/"$WNXVER"/Whonix-"$WNXFAC"-"$WNXVER".libvirt.xz.asc
 torsocks wget -c https://www.whonix.org/hulahoop.asc
 echo "Verifing Keys..."
 gpg --keyid-format long --with-fingerprint hulahoop.asc
 gpg --import hulahoop.asc 
-gpg --verify-options show-notations --verify Whonix-$WNXFAC-$WNXVER.libvirt.xz.asc
-tar -xvf Whonix-$WNXFAC-$WNXVER.libvirt.xz
+gpg --verify-options show-notations --verify Whonix-"$WNXFAC"-"$WNXVER".libvirt.xz.asc
+tar -xvf Whonix-"$WNXFAC"-"$WNXVER".libvirt.xz
 touch WHONIX_BINARY_LICENSE_AGREEMENT_accepted
 
 
 }
 
+## WORKING ON THIS..
 
-DebianQEMUCONT(){  ## WORKING ON THIS..
+DebianQEMUCONT() {  
 virsh -c qemu:///system net-autostart default
 virsh -c qemu:///system net-start default
 sudo virsh autostart linuxconfig-vm
@@ -218,39 +222,42 @@ sudo cp --sparse=always Whonix-Workstation*.qcow2 /var/lib/libvirt/images/Whonix
 
 ##
 echo "Encrypted containers"
-sudo chmod og+xr /run/media/private/user/$container_name
+sudo chmod og+xr /run/media/private/user/"$container_name"
 #rm Downloads/Whonix*
 
-if [ "$WNXFAC" == "CLI" ] then;
-read -p "Will Run Whonix Gateway on CLI...
+if [ "$WNXFAC" == "CLI" ]
+then
+ read -r -p "Will Run Whonix Gateway on CLI...
 Press <Enter> Key to continue..."
-sudo virsh start Whonix-Gateway
+ sudo virsh start Whonix-Gateway
 
-read -p "Will Run Whonix Workstation on CLI...
+ read -r -p "Will Run Whonix Workstation on CLI...
 Press <Enter> Key to continue..."
-systemctl enable serial-getty@ttyS0.service
-systemctl start serial-getty@ttyS0.service
-sudo virsh start Whonix-Workstation
+ systemctl enable serial-getty@ttyS0.service
+ systemctl start serial-getty@ttyS0.service
+ sudo virsh start Whonix-Workstation
 
-cat 'EOF'
+cat <<EOF
 Change this /etc/default/grub on VM Workstation to able console serial
  GRUB_CMDLINE_LINUX_DEFAULT=“console=tty0 console=ttyS0"
  GRUB_TERMINAL=“serial console"
 update-grub
 EOF
-read -p "continue...."
+
+read -r -p "continue...."
 virsh console Whonix-Workstation
 else
-cat 'EOF'
+cat <<EOF
 Go Start - Application - System - Virtual machine Manager
   - Run Gateway
   - Run Workstation
 EOF
 fi
-read -p "Change screen Resolution: "
+read -r -p "Change screen Resolution: "
 ls -la /var/run/libvirt/libvirt-sock
 }
-DebianEnder(){
+
+DebianEnder() {
 sudo systemctl disable tor.service
 
 echo "alias dist-upgrade='sudo systemctl start tor.service && sleep 30 && sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get clean && sudo systemctl stop tor.service && test -f /var/run/reboot-required && tput setaf 1 && echo NEW SOFTWARE REQUIRES A REBOOT. && tput setaf 2 && echo Press ENTER to reboot your && read -sp computer: && echo If prompted by Debian, type your password and press ENTER. && sudo reboot'" >> .bashrc 
@@ -260,10 +267,10 @@ echo "alias vbcompact='find ~/VirtualBox\ VMs/ -type f -mtime -1 -size +10M -nam
 source .bashrc
 dist-upgrade 
 
-read -p "Os Updated!"
+read -r -p "Os Updated!"
 }
 
-Instructer1(){
+Instructer1() {
 cat <<EOF
 ==> Open Oracle Virtual Box
 
@@ -284,7 +291,7 @@ TIP: Run within the respective Whonix VirtualMachines
 -------------------------------------------
 EOF
 
-read -p "Press <ENTER> Key to continue..."
+read -r -p "Press <ENTER> Key to continue..."
 
 cat <<EOF
 2)Restart and Boot Whonix GATEWAY ON Advanced and Recovery Mode
@@ -301,7 +308,7 @@ $ shutdown now
 -----------------------------------------------
 EOF
 
-read -p "Press <ENTER> Key to continue..."
+read -r -p "Press <ENTER> Key to continue..."
 
 cat <<EOF
 5)Restart and Boot Whonix WORKSTATION ON Advanced and Recovery Mode
@@ -332,7 +339,7 @@ THAN SHUTDOWN WHONIX Gateway
 EOF
 }
 
-Instructer2(){
+Instructer2() {
 cat <<EOF
 #############################
 ## BOOT/RUN Whonix Gateway ##
@@ -373,10 +380,10 @@ $ shutdown now
 ##############################################################
 EOF
 
-read -p "### VERIFY WHONIXes VMs ARE ALL SHUTDOWN!!! ###"
+read -r -p "### VERIFY WHONIXes VMs ARE ALL SHUTDOWN!!! ###"
 }
 
-mainstage(){
+mainstage() {
 echo "Detecting OS ..." && sleep 1
 if [ -f /etc/os-release ]
 then
@@ -386,7 +393,7 @@ then
    source Qubes/Qubes.sh ;;
   debian) printf 'Debian Installation\n'
    echo "$NAME $VERSION DETECTED" && sleep 1
-   echo && read -p "What you will use:
+   echo && read -r -p "What you will use:
    1- Virtual Box (working)
    2- Qemu/KVM    (developing...)
    " VMCHOICE
@@ -426,6 +433,7 @@ fi
 # --------------
 # - MAIN STAGE -
 # --------------
+
 anon
 source /etc/os-release
 source ChangeMe
